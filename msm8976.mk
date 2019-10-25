c8525365afc13e3b1614330798f2d00da106d4e6
Cyborg2017 <wangyang0987654@gmail.com>
2019-10-25 10:48:36 +0800

    msm8976-common: light: Use common HIDL light hal

--- msm8976.mk
+++ msm8976.mk
@@ -212,7 +212,7 @@ PRODUCT_COPY_FILES += \
 
 # Lights
 PRODUCT_PACKAGES += \
-    android.hardware.light@2.0-service.nx589j
+    android.hardware.light@2.0-service.msm8976
 
 # Media
 PRODUCT_COPY_FILES += \
