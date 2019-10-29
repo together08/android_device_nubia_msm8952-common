f0bfc7c98d6679b2b26a5cd6effdbd40b5e6aab6
Cyborg2017 <wangyang0987654@gmail.com>
2019-10-30 00:47:12 +0800

    mms8976-common: biometrics: Switch back to use fingerprint 2.0 HAL

--- msm8976.mk
+++ msm8976.mk
@@ -132,7 +132,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.nubia_msm8976
+    android.hardware.biometrics.fingerprint@2.0-service-custom
 
 # Gatekeeper HAL
 PRODUCT_PACKAGES += \
