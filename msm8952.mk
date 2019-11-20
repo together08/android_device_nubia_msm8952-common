8d2c3f98b250dcf60d96545f16121280f7e32251
Kyryll Bondarenko <rolfpeterkesche@googlemail.com>
2019-11-20 05:17:12 +0200

    msm8952-common: Remove hidden API references in ims.apk
    Add telephony-qti-utils libarary to product packages
    
    Change-Id: Ie8651ebadc246c21bd5eb69ef060fe32d8f5f7c8
    (cherry picked from commit 086bbebac59c809999184b2ae44871d1f22b6e8b)

--- msm8952.mk
+++ msm8952.mk
@@ -363,26 +363,14 @@ PRODUCT_PACKAGES += \
 
 # RIL
 PRODUCT_PACKAGES += \
-    android.hardware.radio@1.0 \
-    android.hardware.radio@1.1 \
-    android.hardware.radio.config@1.0 \
-    librmnetctl \
-    libxml2 \
-    libprotobuf-cpp-full
-
-PRODUCT_PACKAGES += \
-    librmnetctl \
-    libxml2 \
-    libqsap_sdk \
-    telephony-ext \
-    libminui
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
+    qti-telephony-utils \
+    qti_telephony_utils.xml \
+    telephony-ext
 
 PRODUCT_BOOT_JARS += \
     telephony-ext
-
-# IMS
-PRODUCT_PACKAGES += \
-    ims-ext-common
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
